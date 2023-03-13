e6fa713cd5b3fa830bf94ca1d5e382f03a93054e
dmjdesu <dmjdesu@gmail.com>
2023-03-13 20:34:48 +0900

    fix url

--- stock/urls.py
+++ stock/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shopping_history', views.ShoppingHistoryView.as_view(), name='stock'),
+    path('shopping_history', views.ShoppingHistoryView.as_view(), name='shopping_history'),
     # path('purchase_history', views.PurchaseHistoryView.as_view(), name='stock'),
 ]
