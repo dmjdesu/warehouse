dd9dfc2040b5dee61c9b7800ca00d6d876699175
dmjdesu <dmjdesu@gmail.com>
2023-03-27 14:14:01 +0900

    fix

--- stock/views.py
+++ stock/views.py
@@ -53,7 +53,7 @@ class ShoppingHistoryView(SuccessMessageMixin,CreateView):
         context = super().get_context_data(**kwargs)
         context['parentcategory_list'] = ParentCategory.objects.all()
         context['item_list'] = Item.objects.all()
-        context['history_list'] = ShoppingHistory.objects.order_by("-date")
+        context['history_list'] = ShoppingHistory.objects.order_by("-updated_at")
         pprint(ShoppingHistory.objects.all()[0].num)
         return context
 
