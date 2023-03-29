e9c174614605eddb1f069c8e836be0eacc516ebf
dmjdesu <dmjdesu@gmail.com>
2023-03-29 17:52:58 +0900

    fix

--- stock/views.py
+++ stock/views.py
@@ -77,67 +77,19 @@ from datetime import date, datetime
 
 class ShoppingPredictionView(View):
     def get(self, request, *args, **kwargs):
-        shopping_data = ShoppingHistory.objects.values('date', 'target_name', 'material_name').annotate(total_num=Sum('num')).order_by('date')
-        df = pd.DataFrame.from_records(shopping_data)
-        df['date'] = pd.to_datetime(df['date'])
-        df['date_ordinal'] = df['date'].apply(lambda x: x.toordinal())
-        min_date_ordinal = df['date_ordinal'].min()
-        df['date_ordinal'] -= min_date_ordinal
-        target_material_pairs = df[['target_name', 'material_name']].drop_duplicates().to_dict('records')
-
-        today_date = datetime.now().date()
-        today_ordinal = today_date.toordinal() - min_date_ordinal
+        df = self.prepare_dataframe()
+        target_material_pairs = self.get_unique_pairs(df)
+        today_ordinal = self.get_today_ordinal(df)
 
         predictions = {}
         model = LinearRegression()
         for pair in target_material_pairs:
-            target_name = pair['target_name']
-            material_name = pair['material_name']
-            target_df = df[(df['target_name'] == target_name) & (df['material_name'] == material_name)]
-            X = target_df[['date_ordinal']]
-            y = target_df['total_num']
-            y_date = target_df['date_ordinal'].shift(-1).dropna()
-            X_date = X.iloc[:-1, :] 
-
-            if len(X) > 5:
-                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-            else:
-                X_train, y_train = X, y
-                X_test, y_test = None, None
-
-            if len(X_date) > 5:
-                X_train_date, X_test_date, y_train_date, y_test_date = train_test_split(X_date, y_date, test_size=0.2, random_state=42)
-            else:
-                X_train_date, y_train_date = X_date, y_date
-                X_test_date, y_test_date = None, None
-
-            model.fit(X_train, y_train)
-
-            if len(X_date) > 0:
-                model_date = LinearRegression()
-                model_date.fit(X_train_date, y_train_date)
-            else:
-                model_date = None
-
-            mse, rmse, y_pred = None, None, None
-            if X_test is not None and y_test is not None:
-                y_pred = model.predict(X_test)
-                mse = mean_squared_error(y_test, y_pred)
-                rmse = np.sqrt(mse)
-
-            latest_date_ordinal = target_df['date_ordinal'].max()
-            next_date_ordinal = max(latest_date_ordinal + 1, today_ordinal)
-            X_next_date = np.array([[next_date_ordinal]])
-            X_next = np.array([[next_date_ordinal]])
-
-            if model_date is not None:
-                next_date_ordinal_pred = round(model_date.predict(X_next_date)[0])
-                next_date_pred = date.fromordinal(next_date_ordinal_pred + min_date_ordinal)
-            else:
-                next_date_pred = today_date
-
-            next_num = round(model.predict(X_next)[0], 4)  # Round to 4 decimal places
-            next_num = max(0, next_num)  # Ensure next_num is not negative
+            target_name, material_name = pair['target_name'], pair['material_name']
+            target_df = self.filter_dataframe(df, target_name, material_name)
+            X, y, X_date, y_date = self.create_train_test_data(target_df)
+            model, model_date = self.fit_models(model, X, y, X_date, y_date)
+            mse, rmse, y_pred = self.calculate_errors(model, X, y)
+            next_date_pred, next_num = self.predict_next_date_and_num(model, model_date, target_df, today_ordinal)
 
             key = f"{target_name}_{material_name}"
             predictions[key] = {
@@ -149,3 +101,76 @@ class ShoppingPredictionView(View):
             }
 
         return render(request, 'stock/predictions.html', {'predictions': predictions})
+
+    def prepare_dataframe(self):
+        shopping_data = ShoppingHistory.objects.values('date', 'target_name', 'material_name').annotate(total_num=Sum('num')).order_by('date')
+        df = pd.DataFrame.from_records(shopping_data)
+        df['date'] = pd.to_datetime(df['date'])
+        df['date_ordinal'] = df['date'].apply(lambda x: x.toordinal())
+        min_date_ordinal = df['date_ordinal'].min()
+        df['date_ordinal'] -= min_date_ordinal
+        return df
+
+    def get_unique_pairs(self,df):
+        return df[['target_name', 'material_name']].drop_duplicates().to_dict('records')
+
+    def get_today_ordinal(self,df):
+        today_date = datetime.now().date()
+        return today_date.toordinal() - df['date_ordinal'].min()
+
+    def filter_dataframe(self,df, target_name, material_name):
+        return df[(df['target_name'] == target_name) & (df['material_name'] == material_name)]
+
+    def create_train_test_data(self,target_df):
+        X = target_df[['date_ordinal']]
+        y = target_df['total_num']
+        y_date = target_df['date_ordinal'].shift(-1).dropna()
+        X_date = X.iloc[:-1, :]
+        return X, y, X_date, y_date
+
+    def fit_models(self,model, X, y, X_date, y_date):
+        X_train, X_test, y_train, y_test = self.train_test_split_with_size_check(X, y)
+        model.fit(X_train, y_train)
+
+        if len(X_date) > 0:
+            model_date = LinearRegression()
+            X_train_date, X_test_date, y_train_date, y_test_date = self.train_test_split_with_size_check(X_date, y_date)
+            model_date.fit(X_train_date, y_train_date)
+        else:
+            model_date = None
+
+        return model, model_date
+
+    def train_test_split_with_size_check(self,X, y, test_size=0.2, random_state=42):
+        if len(X) > 5:
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+        else:
+            X_train, y_train = X, y
+            X_test, y_test = None, None
+        return X_train, X_test, y_train, y_test
+
+    def calculate_errors(self,model, X, y):
+        X_train, X_test, y_train, y_test = self.train_test_split_with_size_check(X, y)
+        mse, rmse, y_pred = None, None, None
+        if X_test is not None and y_test is not None:
+            y_pred = model.predict(X_test)
+            mse = mean_squared_error(y_test, y_pred)
+            rmse = np.sqrt(mse)
+        return mse, rmse, y_pred
+
+    def predict_next_date_and_num(self,model, model_date, target_df, today_ordinal):
+        min_date_ordinal = target_df['date_ordinal'].min()
+        latest_date_ordinal = target_df['date_ordinal'].max()
+        next_date_ordinal = max(latest_date_ordinal + 1, today_ordinal)
+        X_next_date = np.array([[next_date_ordinal]])
+        X_next = np.array([[next_date_ordinal]])
+
+        if model_date is not None:
+            next_date_ordinal_pred = round(model_date.predict(X_next_date)[0])
+            next_date_pred = date.fromordinal(next_date_ordinal_pred + min_date_ordinal)
+        else:
+            next_date_pred = date.fromordinal(today_ordinal + min_date_ordinal)
+
+        next_num = round(model.predict(X_next)[0], 4)  # Round to 4 decimal places
+        next_num = max(0, next_num)  # Ensure next_num is not negative
+        return next_date_pred, next_num
