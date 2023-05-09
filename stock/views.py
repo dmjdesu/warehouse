c574967835db0848ed1df39a8b5be0bb0c32e8aa
dmjdesu <dmjdesu@gmail.com>
2023-05-09 17:09:04 +0900

    fix

--- stock/views.py
+++ stock/views.py
@@ -46,8 +46,8 @@ class ShoppingHistoryView(SuccessMessageMixin,CreateView):
             try:
                 ShoppingHistory.objects.create(
                     target_name = request.POST["target_name"],
-                    value=round(Decimal(request.POST["num"]) * (material.value + material.extra),4),
-                    tax_value=round(Decimal(request.POST["num"]) * (material.value),4),
+                    value=round(Decimal(request.POST["num"]) * (material.value ),4),
+                    tax_value=round(Decimal(request.POST["num"]) * (material.value + material.extra ),4),
                     num=(Decimal(request.POST["num"])),
                     material_name=material.name,
                     material_item_name=material.item.name,
