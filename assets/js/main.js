9806b05f7a9897cc0b96455c85ee2b7bd1b21efa
dmjdesu <dmjdesu@gmail.com>
2023-06-12 08:44:54 +0900

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 import '@popperjs/core';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import '../css/index.css'
+// import '../css/index.css'
 
 import App from './App.jsx';
 
