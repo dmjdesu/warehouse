e60fee4f687abd3e8f57c391eebd579f791de8b5
dmjdesu <dmjdesu@gmail.com>
2023-06-30 15:36:50 +0900

    fix

--- assets/js/App.jsx
+++ assets/js/App.jsx
@@ -282,7 +282,7 @@ const handleTodayBlur = (e, materialId,num) => {
         className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
         <FaCalendarAlt />
-        Choise date
+        Choice date
       </button>
         
       }
