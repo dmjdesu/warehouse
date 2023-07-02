2ee1b1337f8a10d1d4b699defee59ae179727cfc
dmjdesu <dmjdesu@gmail.com>
2023-07-02 16:25:19 +0900

    fix

--- shoping/urls.py
+++ shoping/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-admin.site.site_title = 'KOYAグループ在庫管理システム' 
-admin.site.site_header = 'KOYAグループ在庫管理システム' 
+admin.site.site_title = 'Kizuna Enterpriseグループ在庫管理システム' 
+admin.site.site_header = 'Kizuna Enterpriseグループ在庫管理システム' 
 admin.site.index_title = 'メニュー'
 
 urlpatterns = [
